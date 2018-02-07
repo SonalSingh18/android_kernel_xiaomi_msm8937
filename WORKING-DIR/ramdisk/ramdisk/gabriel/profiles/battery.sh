6749a4332d3b60187dc68b61b24a9316e12a99b5
mostafa-z <mostafazarghami@gmail.com>
2018-02-08 01:50:14 +0330

    ramdisk: spectrum: use flexible scripts
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

---
+++ WORKING-DIR/ramdisk/ramdisk/gabriel/profiles/battery.sh
@@ -0,0 +1,83 @@
+#!/system/bin/sh
+# created by mostafaz@xda
+
+(
+sleep 30;
+
+GOV=interactive
+TARGET0="65 960000:75 1094400:80 1209600:99 1248000:97 1344000:99 1401000:99"
+TARGET4="71 768000:70 902400:77 998400:87 1094400:99"
+
+for i in 0 1 2 3; do
+	CPUB=sys/devices/system/cpu/cpu$i/cpufreq
+    if [ "$(cat /sys/devices/system/cpu/cpu$i/online)" == "1" ];then
+    	chmod 644 /$CPUB/scaling_governor
+	    echo "interactive" > /$CPUB/scaling_governor
+	    chmod 444 /$CPUB/scaling_governor
+	    chmod 644 /$CPUB/$GOV/*
+	    echo 0 > /$CPUB/$GOV/use_sched_load
+	    echo 1 > /$CPUB/$GOV/use_migration_notif
+	    echo 0 > /$CPUB/$GOV/enable_prediction
+	    echo "20000 1094400:20000 1209600:20000 1401000:39000" > /$CPUB/$GOV/above_hispeed_delay
+	    echo 400 > /$CPUB/$GOV/go_hispeed_load
+	    echo 960000 > /$CPUB/$GOV/hispeed_freq
+	    echo "$TARGET0" > /$CPUB/$GOV/target_loads
+	    echo 96666 > /$CPUB/$GOV/timer_rate
+	    echo 11000 > /$CPUB/$GOV/min_sample_time
+	    echo 1 > /$CPUB/$GOV/ignore_hispeed_on_notif
+	    echo 0 > /$CPUB/$GOV/max_freq_hysteresis
+	    echo 160000 > /$CPUB/$GOV/timer_slack
+	    echo 0 > /$CPUB/$GOV/io_is_busy
+    	echo 0 > /$CPUL/$GOV/boost
+    	echo 0 > /$CPUL/$GOV/boostpulse_duration
+	    echo 1 > /$CPUB/$GOV/fast_ramp_down
+    	echo 1248000 > /$CPUB/scaling_max_freq
+        break
+    fi;
+done;
+
+for i in 4 5 6 7; do
+	CPUL=sys/devices/system/cpu/cpu$i/cpufreq
+    if [ "$(cat /sys/devices/system/cpu/cpu$i/online)" == "1" ];then
+    	chmod 644 /$CPUL/scaling_governor
+    	echo "interactive" > /$CPUL/scaling_governor
+    	chmod 444 /$CPUL/scaling_governor
+    	chmod 644 /$CPUL/$GOV/*
+    	echo 0 > /$CPUL/$GOV/use_sched_load
+    	echo 1 > /$CPUL/$GOV/use_migration_notif
+    	echo 0 > /$CPUL/$GOV/enable_prediction
+    	echo 10000 > /$CPUL/$GOV/above_hispeed_delay
+    	echo 155 > /$CPUL/$GOV/go_hispeed_load
+    	echo 768000 > /$CPUL/$GOV/hispeed_freq
+    	echo "$TARGET4" > /$CPUL/$GOV/target_loads
+    	echo 86666 > /$CPUL/$GOV/timer_rate
+    	echo 23000 > /$CPUL/$GOV/min_sample_time
+    	echo 0 > /$CPUL/$GOV/ignore_hispeed_on_notif
+    	echo 0 > /$CPUL/$GOV/max_freq_hysteresis
+    	echo 145000 > /$CPUL/$GOV/timer_slack
+    	echo 0 > /$CPUL/$GOV/io_is_busy
+    	echo 1 > /$CPUL/$GOV/boost
+    	echo 0 > /$CPUL/$GOV/boostpulse_duration
+    	echo 0 > /$CPUL/$GOV/fast_ramp_down
+    	echo 998400 > /$CPUL/scaling_max_freq
+        break
+    fi;
+done;
+
+echo 1 > /sys/module/msm_thermal/core_control/enabled
+echo 0 > /sys/module/msm_thermal/parameters/enabled
+echo "powersave" > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+echo 160000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+echo 375000000 > /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+echo 8 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/device/kgsl/kgsl-3d0/default_pwrlevel
+echo 1 > /sys/module/adreno_idler/parameters/adreno_idler_active
+echo 15 > /sys/module/adreno_idler/parameters/adreno_idler_idlewait
+echo 40 > /sys/module/adreno_idler/parameters/adreno_idler_downdifferential
+echo 5000 > /sys/module/adreno_idler/parameters/adreno_idler_idleworkload
+echo "noop"> /sys/block/mmcblk0/queue/scheduler
+echo 1 > /sys/module/workqueue/parameters/power_efficient
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lm
+echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
+
+echo "[Gabriel-Kernel] spectrum: battery profile loaded" > /dev/kmsg
+)&
---
+++ WORKING-DIR/ramdisk/ramdisk/gabriel/profiles/gaming.sh
@@ -0,0 +1,83 @@
+#!/system/bin/sh
+# created by mostafaz@xda
+
+(
+sleep 30;
+
+GOV=interactive
+TARGET0="45 1248000:75"
+TARGET4="50 902400:69"
+
+for i in 0 1 2 3; do
+	CPUB=sys/devices/system/cpu/cpu$i/cpufreq
+    if [ "$(cat /sys/devices/system/cpu/cpu$i/online)" == "1" ];then
+    	chmod 644 /$CPUB/scaling_governor
+	    echo "interactive" > /$CPUB/scaling_governor
+	    chmod 444 /$CPUB/scaling_governor
+	    chmod 644 /$CPUB/$GOV/*
+	    echo 1 > /$CPUB/$GOV/use_sched_load
+	    echo 1 > /$CPUB/$GOV/use_migration_notif
+	    echo 1 > /$CPUB/$GOV/enable_prediction
+	    echo 0 > /$CPUB/$GOV/above_hispeed_delay
+	    echo 85 > /$CPUB/$GOV/go_hispeed_load
+	    echo 1344000 > /$CPUB/$GOV/hispeed_freq
+	    echo "$TARGET0" > /$CPUB/$GOV/target_loads
+	    echo 20000 > /$CPUB/$GOV/timer_rate
+	    echo 60000 > /$CPUB/$GOV/min_sample_time
+	    echo 1 > /$CPUB/$GOV/ignore_hispeed_on_notif
+	    echo 79000 > /$CPUB/$GOV/max_freq_hysteresis
+	    echo 80000 > /$CPUB/$GOV/timer_slack
+	    echo 0 > /$CPUB/$GOV/io_is_busy
+    	echo 0 > /$CPUB/$GOV/boost
+    	echo 0 > /$CPUB/$GOV/boostpulse_duration
+	    echo 0 > /$CPUB/$GOV/fast_ramp_down
+    	echo 1401000 > /$CPUB/scaling_max_freq
+        break
+    fi;
+done;
+
+for i in 4 5 6 7; do
+	CPUL=sys/devices/system/cpu/cpu$i/cpufreq
+    if [ "$(cat /sys/devices/system/cpu/cpu$i/online)" == "1" ];then
+    	chmod 644 /$CPUL/scaling_governor
+    	echo "interactive" > /$CPUL/scaling_governor
+    	chmod 444 /$CPUL/scaling_governor
+    	chmod 644 /$CPUL/$GOV/*
+    	echo 1 > /$CPUL/$GOV/use_sched_load
+    	echo 1 > /$CPUL/$GOV/use_migration_notif
+    	echo 0 > /$CPUL/$GOV/enable_prediction
+    	echo 0 > /$CPUL/$GOV/above_hispeed_delay
+    	echo 75 > /$CPUL/$GOV/go_hispeed_load
+    	echo 998400 > /$CPUL/$GOV/hispeed_freq
+    	echo "$TARGET4" > /$CPUL/$GOV/target_loads
+    	echo 20000 > /$CPUL/$GOV/timer_rate
+    	echo 40000 > /$CPUL/$GOV/min_sample_time
+    	echo 1 > /$CPUL/$GOV/ignore_hispeed_on_notif
+    	echo 79000 > /$CPUL/$GOV/max_freq_hysteresis
+    	echo 80000 > /$CPUL/$GOV/timer_slack
+    	echo 0 > /$CPUL/$GOV/io_is_busy
+    	echo 0 > /$CPUL/$GOV/boost
+    	echo 0 > /$CPUL/$GOV/boostpulse_duration
+    	echo 0 > /$CPUL/$GOV/fast_ramp_down
+    	echo 1094400 > /$CPUL/scaling_max_freq
+        break
+    fi;
+done;
+
+echo 1 > /sys/module/msm_thermal/core_control/enabled
+echo 0 > /sys/module/msm_thermal/parameters/enabled
+echo "msm-adreno-tz" > /sys/class/kgsl/kgsl-3d0/devfreq/governor
+echo 375000000 > /sys/class/kgsl/kgsl-3d0/devfreq/min_freq
+echo 475000000 > /sys/class/kgsl/kgsl-3d0/devfreq/max_freq
+echo 3 > /sys/class/devfreq/1c00000.qcom,kgsl-3d0/device/kgsl/kgsl-3d0/default_pwrlevel
+echo 0 > /sys/module/adreno_idler/parameters/adreno_idler_active
+echo 15 > /sys/module/adreno_idler/parameters/adreno_idler_idlewait
+echo 40 > /sys/module/adreno_idler/parameters/adreno_idler_downdifferential
+echo 5000 > /sys/module/adreno_idler/parameters/adreno_idler_idleworkload
+echo "deadline"> /sys/block/mmcblk0/queue/scheduler
+echo 0 > /sys/module/workqueue/parameters/power_efficient
+echo 0 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lm
+echo "westwood" > /proc/sys/net/ipv4/tcp_congestion_control
+
+echo "[Gabriel-Kernel] spectrum: gaming profile loaded" > /dev/kmsg
+)&
